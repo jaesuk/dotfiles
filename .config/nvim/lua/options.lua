882fdb81ae09026c67eab2a51ee70755160d9d32
Jaesuk Hwang <{ID}+{username}@users.noreply.github.com>
2024-08-18 19:11:55 +0900

    Add to use NvChad and go.nvim

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,15 @@
+require "nvchad.options"
+
+local o = vim.o
+
+o.encoding = "utf-8"
+o.fileencoding = "utf-8"
+
+o.backup = false
+o.swapfile = false
+
+o.scrolloff = 10
+o.relativenumber = true
+o.wrap = true
+
+o.cursorlineopt = "both"
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    opts = require "configs.conform",
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
+    },
+  },
+
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()',
+  },
+}
